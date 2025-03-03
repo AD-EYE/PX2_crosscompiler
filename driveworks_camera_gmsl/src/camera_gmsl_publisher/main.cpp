e245bee2e5b18769b2ac212f0b307345c7d445c9
iozgen <iozgen@kth.se>
2025-03-03 14:27:06 +0100

    Add image scaling capability to camera node

--- driveworks_camera_gmsl/src/camera_gmsl_publisher/main.cpp
+++ driveworks_camera_gmsl/src/camera_gmsl_publisher/main.cpp
@@ -1,4 +1,3 @@
-
 // ROS
 #include "ros/ros.h"
 #include "std_msgs/String.h"
@@ -19,7 +18,6 @@
 
 // Image
 #include <dw/image/ImageStreamer.h>
-
 #include <dw/image/FormatConverter_vibrante.h>
 
 // nvmedia for surface map
@@ -32,9 +30,7 @@
 #include <dw/renderer/Renderer.h>
 
 #include <sstream>
-
 #include <boost/program_options.hpp>
-
 
 #define CHECK_DW_ERROR(x) { \
                     dwStatus result = x; \
@@ -50,19 +46,16 @@ namespace po = boost::program_options;
 class CameraGMSL
 {
 private:
-    // ------------------------------------------------
     // Driveworks Context and SAL
-    // ------------------------------------------------
-    dwContextHandle_t sdk_                  = DW_NULL_HANDLE;
-    dwSALHandle_t sal_                      = DW_NULL_HANDLE;
+    dwContextHandle_t SDK_ = DW_NULL_HANDLE;
+    dwSALHandle_t SAL_ = DW_NULL_HANDLE;
     
     // ROS variables 
     ros::Publisher gmsl_pub_, gmsl_pub_img_;
     sensor_msgs::ImagePtr ros_img_ptr_; 
 
-
     // Image handles and properties
-    dwImageHandle_t frame_rgb_ = DW_NULL_HANDLE; 
+    dwImageHandle_t frame_resized_ = DW_NULL_HANDLE; // Resized image handle
     dwSensorHandle_t camera_ = DW_NULL_HANDLE;
     dwImageProperties camera_image_properties_;
     dwCameraProperties camera_properties_;
@@ -70,53 +63,33 @@ private:
     po::variables_map args_;
      
 public:
-    CameraGMSL(const po::variables_map args): args_(args)
+    CameraGMSL(const po::variables_map args) : args_(args)
     {
-
-    
-        // -----------------------------------------
         // Initialize DriveWorks context and SAL
-        // -----------------------------------------
         {
-            // instantiate Driveworks SDK context
-            dwContextParameters sdk_params = {};
-            CHECK_DW_ERROR(dwInitialize(&sdk_, DW_VERSION, &sdk_params));
-
-            // create HAL module of the SDK
-            CHECK_DW_ERROR(dwSAL_initialize(&sal_, sdk_));
-
+            dwContextParameters SDK_params = {};
+            CHECK_DW_ERROR(dwInitialize(&SDK_, DW_VERSION, &SDK_params));
+            CHECK_DW_ERROR(dwSAL_initialize(&SAL_, SDK_));
         }
 
-        //------------------------------------------------------------------------------
-        // initializes camera
-        // - the SensorCamera module
-        // -----------------------------------------
+        // Initialize camera
         {
-
-
             dwSensorParams params;
-            std::string parameter_string = std::string("output-format=yuv,fifo-size=3");
+            std::string parameter_string = "output-format=yuv,fifo-size=3";
+            parameter_string += ",camera-type=" + args_["camera-type"].as<std::string>();
+            parameter_string += ",csi-port=" + args_["camera-port"].as<std::string>();
+            parameter_string += ",slave=" + args_["tegra-slave"].as<std::string>();
 
-            parameter_string             += std::string(",camera-type=") + args_["camera-type"].as<std::string>().c_str();
-            parameter_string             += std::string(",csi-port=") + args_["camera-port"].as<std::string>().c_str();
-            parameter_string             += std::string(",slave=") + args_["tegra-slave"].as<std::string>().c_str();
-
-            if (args_["custom-board"].as<std::string>().compare("1") == 0)
+            if (args_["custom-board"].as<std::string>() == "1")
             {
-                // it's a custom board, use the board specific extra configurations
-                parameter_string             += ",custom-board=1";
-
-                // pass an extra set of parameter in custom-config
-                params.auxiliarydata           = args_["custom-board"].as<std::string>().c_str();
+                parameter_string += ",custom-board=1";
+                params.auxiliarydata = args_["custom-board"].as<std::string>().c_str();
             }
 
-            params.parameters           = parameter_string.c_str();
-            params.protocol             = "camera.gmsl";
+            params.parameters = parameter_string.c_str();
+            params.protocol = "camera.gmsl";
 
-            CHECK_DW_ERROR(dwSAL_createSensor(&camera_, params, sal_));
-
-            // sensor can take some time to start, it's possible to call the read function and check if the return status is ok
-            // before proceding
+            CHECK_DW_ERROR(dwSAL_createSensor(&camera_, params, SAL_));
             CHECK_DW_ERROR(dwSensor_start(camera_));
 
             dwCameraFrameHandle_t frame;
@@ -125,41 +98,44 @@ public:
                 status = dwSensorCamera_readFrame(&frame, 0, 66000, camera_);
             } while (status == DW_NOT_READY);
 
-            // something wrong happened, aborting
             if (status != DW_SUCCESS) {
                 throw std::runtime_error("Cameras did not start correctly");
             }
 
             CHECK_DW_ERROR(dwSensorCamera_returnFrame(&frame));
-
             CHECK_DW_ERROR(dwSensorCamera_getSensorProperties(&camera_properties_, camera_));
             ROS_INFO("Successfully initialized camera with resolution of %dx%d at framerate of %f FPS\n",
                 camera_properties_.resolution.x, camera_properties_.resolution.y, camera_properties_.framerate);
-
         }
 
-         //ROS initialization
+        // ROS initialization with parameter reading
         {
-            ros::VP_string ros_str;
             ros::init(ros_str, "camera_gmsl");
             ros::NodeHandle n;
             gmsl_pub_img_ = n.advertise<sensor_msgs::Image>("camera_1/image_raw", 1);
-
             ros_img_ptr_ = boost::make_shared<sensor_msgs::Image>();
-            ROS_INFO("Successfully initialized ros\n" );
-        }
+            ROS_INFO("Successfully initialized ros\n");
 
-        //Nvmedia initialization
-        {
-            dwImageProperties rgb_img_prop{};
-            rgb_img_prop.height = camera_properties_.resolution.y;
-            rgb_img_prop.width = camera_properties_.resolution.x;
-            rgb_img_prop.type = DW_IMAGE_NVMEDIA;
-            rgb_img_prop.format = DW_IMAGE_FORMAT_RGBA_UINT8;
-            CHECK_DW_ERROR(dwImage_create(&frame_rgb_,  rgb_img_prop,sdk_	));
-            ROS_INFO("Successfully initialized nvmedia img.\n" );
+            double scaling_factor;
+            if (!n.getParam("scaling_factor", scaling_factor)) {
+                scaling_factor = 1.0; // default, no scaling
+            }
+            if (scaling_factor <= 0 || scaling_factor > 1) {
+                ROS_ERROR("Invalid scaling_factor. Using default 1.0");
+                scaling_factor = 1.0;
+            }
+            int new_width = static_cast<int>(camera_properties_.resolution.x * scaling_factor);
+            int new_height = static_cast<int>(camera_properties_.resolution.y * scaling_factor);
+
+            // Create resized image handle
+            dwImageProperties resized_prop{};
+            resized_prop.height = new_height;
+            resized_prop.width = new_width;
+            resized_prop.type = DW_IMAGE_NVMedia;
+            resized_prop.format = DW_IMAGE_FORMAT_RGBA_UINT8;
+            CHECK_DW_ERROR(dwImage_create(&frame_resized_, resized_prop, SDK_));
+            ROS_INFO("Successfully initialized resized img with resolution %dx%d\n", new_width, new_height);
         }
-        
     }
 
     ~CameraGMSL()
@@ -170,13 +146,12 @@ public:
             dwSAL_releaseSensor(&camera_);
         }
 
-        //destroy created image
-        dwImage_destroy(&frame_rgb_);
+        // Destroy resized image
+        dwImage_destroy(&frame_resized_);
 
-        dwSAL_release(&sal_);
-        dwRelease(&sdk_);
+        dwSAL_release(&SAL_);
+        dwRelease(&SDK_);
         dwLogger_release();
-
     }
 
     void publish()
@@ -198,25 +173,21 @@ public:
                 dwImageNvMedia* nvmedia_yuv_img_ptr;
                 dwImageNvMedia* nvmedia_rgb_img_ptr;
 
-                sensor_msgs::Image &img_msg = *ros_img_ptr_; // >> message to be sent
+                sensor_msgs::Image &img_msg = *ros_img_ptr_; // message to be sent
                 std_msgs::Header header; // empty header
                 size_t img_size;
 
-                
-                // read from camera will update the low level buffers frame of the camera
-                // those frames are images with NATIVE properties that depend on the type and sensor properties set at creation
+                // Read from camera
                 CHECK_DW_ERROR(dwSensorCamera_readFrame(&frame, camera_sibling_id, timeout, camera_));
 
                 CHECK_DW_ERROR(dwSensorCamera_getImageNvMedia(&nvmedia_yuv_img_ptr, DW_CAMERA_OUTPUT_NATIVE_PROCESSED, frame));
-                CHECK_DW_ERROR(dwImage_getNvMedia(&nvmedia_rgb_img_ptr, frame_rgb_));
                 CHECK_DW_ERROR(dwImage_createAndBindNvMedia(&frame_yuv, nvmedia_yuv_img_ptr->img));
-                CHECK_DW_ERROR(dwImage_copyConvert(frame_rgb_, frame_yuv, sdk_));
-                CHECK_DW_ERROR(dwImage_getNvMedia(&nvmedia_rgb_img_ptr, frame_rgb_));
+                CHECK_DW_ERROR(dwImage_copyConvert(frame_resized_, frame_yuv, SDK_));
+                CHECK_DW_ERROR(dwImage_getNvMedia(&nvmedia_rgb_img_ptr, frame_resized_));
 
+                header.seq = count;
+                header.stamp = ros::Time::now();
                 
-                header.seq = count; // user defined counter
-                header.stamp = ros::Time::now(); 
-                    
                 img_msg.header = header;
                 img_msg.height = nvmedia_rgb_img_ptr->prop.height;
                 img_msg.width = nvmedia_rgb_img_ptr->prop.width;
@@ -226,17 +197,17 @@ public:
 
                 img_size = img_msg.step * img_msg.height;
                 img_msg.data.resize(img_size);
-                    NvMediaImageSurfaceMap surfaceMap;
+                NvMediaImageSurfaceMap surfaceMap;
                 if (NvMediaImageLock(nvmedia_rgb_img_ptr->img, NVMEDIA_IMAGE_ACCESS_READ, &surfaceMap) == NVMEDIA_STATUS_OK)
                 {
-                        unsigned char* buffer = (unsigned char*)surfaceMap.surface[0].mapping;
-                        memcpy((char *)( &img_msg.data[0] ) , buffer , img_size);
-                        gmsl_pub_img_.publish(ros_img_ptr_);
-                        NvMediaImageUnlock(nvmedia_rgb_img_ptr->img);
+                    unsigned char* buffer = (unsigned char*)surfaceMap.surface[0].mapping;
+                    memcpy(&img_msg.data[0], buffer, img_size);
+                    gmsl_pub_img_.publish(ros_img_ptr_);
+                    NvMediaImageUnlock(nvmedia_rgb_img_ptr->img);
                 }
-                //   cleanup
+
+                // Cleanup
                 CHECK_DW_ERROR(dwImage_destroy(&frame_yuv));       
-                // return frame
                 CHECK_DW_ERROR(dwSensorCamera_returnFrame(&frame));
                 ros::spinOnce();
                 loop_rate.sleep();
@@ -247,15 +218,12 @@ public:
         {
             std::cerr << e.what() << "\n";
         }
-        
     }
-
 };
 
 //------------------------------------------------------------------------------
 int main(int argc, const char *argv[])
 {
-
     po::options_description desc{"Options"};
     desc.add_options()
         ("help,h", "Help screen")
