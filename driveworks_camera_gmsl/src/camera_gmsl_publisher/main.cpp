c408f348274e7ed9d289f011967af29d4fb37a8d
iozgen <iozgen@kth.se>
2025-03-14 12:28:38 +0100

    try20

--- driveworks_camera_gmsl/src/camera_gmsl_publisher/main.cpp
+++ driveworks_camera_gmsl/src/camera_gmsl_publisher/main.cpp
@@ -1,190 +1,172 @@
 #include "ros/ros.h"
-#include "std_msgs/String.h"
 #include "sensor_msgs/Image.h"
 #include "sensor_msgs/image_encodings.h"
-#include "ros/console.h"
 
-// DriveWorks ve NVMedia başlıkları
 #include <dw/core/Context.h>
 #include <dw/core/Logger.h>
+#include <dw/core/VersionCurrent.h>   // DW_VERSION tanımı için gerekli
 #include <dw/core/NvMedia.h>
 #include <dw/sensors/Sensors.h>
 #include <dw/sensors/SensorSerializer.h>
 #include <dw/sensors/camera/Camera.h>
 #include <dw/image/ImageStreamer.h>
 #include <dw/image/FormatConverter_vibrante.h>
+
 #include <nvmedia_2d.h>
 #include "nvmedia_image.h"
 #include "nvmedia_ijpe.h"
 #include "nvmedia_surface.h"
-#include <dw/renderer/Renderer.h>
 
-#include <sstream>
-#include <stdexcept>
+#include <dw/renderer/Renderer.h>
 #include <boost/program_options.hpp>
+#include <opencv2/opencv.hpp>
+#include <stdexcept>
+#include <sstream>
 
 #define CHECK_DW_ERROR(x) { \
     dwStatus result = x; \
     if(result != DW_SUCCESS) { \
-        throw std::runtime_error(std::string("DW Error ") + dwGetStatusName(result) + \
-                                 " executing DW function: " #x); \
+        throw std::runtime_error(std::string("DW Error ") + dwGetStatusName(result) + " executing DW function: " #x); \
     } \
 }
 
 namespace po = boost::program_options;
 
-// Örnek sınıf; sensör başlatma, dönüşüm ve ROS yayınlama işlemleri bu sınıf içerisinde gerçekleştiriliyor.
-class CameraGMSL
-{
+class CameraGMSL {
 private:
-    // DriveWorks context ve SAL
     dwContextHandle_t sdk_ = DW_NULL_HANDLE;
-    dwSALHandle_t sal_     = DW_NULL_HANDLE;
+    dwSALHandle_t sal_ = DW_NULL_HANDLE;
     dwSensorHandle_t camera_ = DW_NULL_HANDLE;
     dwCameraProperties camera_properties_;
 
-    // ROS publisher ve mesaj
     ros::Publisher gmsl_pub_img_;
     sensor_msgs::ImagePtr ros_img_ptr_;
 
-    // İki aşamalı dönüşüm için NVMedia görüntü tanıtıcıları:
-    dwImageHandle_t frame_rgb_native_ = DW_NULL_HANDLE;  // native çözünürlükte RGBA8 görüntü (1920×1208)
-    dwImageHandle_t frame_rgb_resized_ = DW_NULL_HANDLE;   // ölçeklenmiş görüntü (640×480)
+    // Native görüntü, sensörden alınan orijinal çözünürlükte (örneğin 1920x1208) oluşturuluyor
+    dwImageHandle_t frame_rgb_native_ = DW_NULL_HANDLE;
 
     po::variables_map args_;
 
 public:
-    CameraGMSL(const po::variables_map &args) : args_(args)
-    {
-        // DriveWorks context ve SAL başlatma
+    CameraGMSL(const po::variables_map &args) : args_(args) {
+        // DriveWorks context başlatılıyor
         dwContextParameters sdk_params = {};
         CHECK_DW_ERROR(dwInitialize(&sdk_, DW_VERSION, &sdk_params));
         CHECK_DW_ERROR(dwSAL_initialize(&sal_, sdk_));
 
-        // Sensör parametreleri oluşturuluyor
+        // Sensör parametreleri hazırlanıyor
         dwSensorParams params;
-        std::string parameter_string = std::string("output-format=yuv,fifo-size=3");
-        parameter_string += std::string(",camera-type=") + args_["camera-type"].as<std::string>();
-        parameter_string += std::string(",csi-port=") + args_["camera-port"].as<std::string>();
-        parameter_string += std::string(",slave=") + args_["tegra-slave"].as<std::string>();
+        std::string parameter_string = "output-format=yuv,fifo-size=3";
+        parameter_string += ",camera-type=" + args_["camera-type"].as<std::string>();
+        parameter_string += ",csi-port=" + args_["camera-port"].as<std::string>();
+        parameter_string += ",slave=" + args_["tegra-slave"].as<std::string>();
 
-        if(args_["custom-board"].as<std::string>().compare("1") == 0)
-        {
+        if (args_["custom-board"].as<std::string>() == "1") {
             parameter_string += ",custom-board=1";
             params.auxiliarydata = args_["custom-config"].as<std::string>().c_str();
         }
         params.parameters = parameter_string.c_str();
-        params.protocol   = "camera.gmsl";
+        params.protocol = "camera.gmsl";
 
         CHECK_DW_ERROR(dwSAL_createSensor(&camera_, params, sal_));
         CHECK_DW_ERROR(dwSensor_start(camera_));
 
-        // Sensörün başlaması için ilk frame okunuyor
+        // Sensörden ilk frame alınarak sensör başlatılıyor
         dwCameraFrameHandle_t frame;
         dwStatus status = DW_NOT_READY;
         do {
             status = dwSensorCamera_readFrame(&frame, 0, 66000, camera_);
         } while(status == DW_NOT_READY);
-        if(status != DW_SUCCESS)
-        {
+        if(status != DW_SUCCESS) {
             throw std::runtime_error("Cameras did not start correctly");
         }
         CHECK_DW_ERROR(dwSensorCamera_returnFrame(&frame));
 
-        // Sensör özelliklerinin alınması
+        // Sensör özellikleri alınıyor
         CHECK_DW_ERROR(dwSensorCamera_getSensorProperties(&camera_properties_, camera_));
-        ROS_INFO("Sensor resolution: %dx%d at %f FPS", camera_properties_.resolution.x, camera_properties_.resolution.y, camera_properties_.framerate);
+        ROS_INFO("Sensor resolution: %dx%d at %f FPS", 
+                 camera_properties_.resolution.x, camera_properties_.resolution.y, camera_properties_.framerate);
 
-        // ROS initialization
+        // ROS başlatılıyor
         ros::NodeHandle n;
-        gmsl_pub_img_ = n.advertise<sensor_msgs::Image>("camera_1/image_raw_cgResize", 1);
+        gmsl_pub_img_ = n.advertise<sensor_msgs::Image>("camera_1/image_raw_cg2Open", 1);
         ros_img_ptr_ = boost::make_shared<sensor_msgs::Image>();
 
-        // --- Native görüntü oluşturulması ---
+        // Native NVMedia görüntü, sensörün orijinal çözünürlüğüyle (örneğin 1920x1208) oluşturuluyor
         dwImageProperties native_prop{};
-        native_prop.width  = camera_properties_.resolution.x;  // 1920
-        native_prop.height = camera_properties_.resolution.y;  // 1208
+        native_prop.width  = camera_properties_.resolution.x;
+        native_prop.height = camera_properties_.resolution.y;
         native_prop.type   = DW_IMAGE_NVMEDIA;
         native_prop.format = DW_IMAGE_FORMAT_RGBA_UINT8;
         CHECK_DW_ERROR(dwImage_create(&frame_rgb_native_, native_prop, sdk_));
         ROS_INFO("Native NVMedia image created: %dx%d", native_prop.width, native_prop.height);
-
-        // --- Ölçeklenmiş görüntü oluşturulması (640x480) ---
-        dwImageProperties resized_prop{};
-        resized_prop.width  = 640;
-        resized_prop.height = 480;
-        resized_prop.type   = DW_IMAGE_NVMEDIA;
-        resized_prop.format = DW_IMAGE_FORMAT_RGBA_UINT8;
-        CHECK_DW_ERROR(dwImage_create(&frame_rgb_resized_, resized_prop, sdk_));
-        ROS_INFO("Resized NVMedia image created: %dx%d", resized_prop.width, resized_prop.height);
     }
 
-    ~CameraGMSL()
-    {
+    ~CameraGMSL() {
         ROS_INFO("Destructor called, cleaning up...");
-        if(camera_)
-        {
+        if(camera_) {
             dwSensor_stop(camera_);
             dwSAL_releaseSensor(&camera_);
         }
         if(frame_rgb_native_) { dwImage_destroy(&frame_rgb_native_); }
-        if(frame_rgb_resized_) { dwImage_destroy(&frame_rgb_resized_); }
         dwSAL_release(&sal_);
         dwRelease(&sdk_);
         dwLogger_release();
     }
 
-    void publish()
-    {
+    void publish() {
         ros::Rate loop_rate(15);
         int count = 0;
-        while(ros::ok())
-        {
-            try
-            {
+        while(ros::ok()) {
+            try {
                 dwCameraFrameHandle_t frame;
                 uint32_t camera_sibling_id = 0;
                 dwImageHandle_t frame_yuv = DW_NULL_HANDLE;
 
-                // Kameradan frame okuma
+                // Kameradan frame alınıyor
                 CHECK_DW_ERROR(dwSensorCamera_readFrame(&frame, camera_sibling_id, 132000, camera_));
 
-                // YUV görüntüyü NVMedia üzerinden alıyoruz
+                // YUV formatındaki görüntü NVMedia üzerinden alınıyor
                 dwImageNvMedia* nvmedia_yuv_img_ptr = nullptr;
                 CHECK_DW_ERROR(dwSensorCamera_getImageNvMedia(&nvmedia_yuv_img_ptr, DW_CAMERA_OUTPUT_NATIVE_PROCESSED, frame));
 
-                // frame_yuv oluşturulup NVMedia imajına bağlanıyor
+                // frame_yuv, NVMedia imajına bağlanıyor
                 CHECK_DW_ERROR(dwImage_createAndBindNvMedia(&frame_yuv, nvmedia_yuv_img_ptr->img));
 
-                // --- AŞAMA 1: YUV görüntüyü native (1920x1208) çözünürlükte RGBA8 formatına dönüştürme ---
+                // --- AŞAMA 1: YUV görüntü native (orijinal) çözünürlükte RGBA8 formatına dönüştürülüyor ---
                 CHECK_DW_ERROR(dwImage_copyConvert(frame_rgb_native_, frame_yuv, sdk_));
 
-                // --- AŞAMA 2: Native görüntüyü 640x480 çözünürlüğe ölçekleme ---
-                CHECK_DW_ERROR(dwImage_resize(frame_rgb_resized_, frame_rgb_native_, sdk_));
-
-                // ROS mesajına aktarım için NVMedia görüntüyü alıyoruz
-                dwImageNvMedia* nvmedia_rgb_img_ptr = nullptr;
-                CHECK_DW_ERROR(dwImage_getNvMedia(&nvmedia_rgb_img_ptr, frame_rgb_resized_));
+                // --- AŞAMA 2: OpenCV kullanılarak native görüntü 640x480 boyutuna yeniden ölçeklendiriliyor ---
+                dwImageNvMedia* nvmedia_rgb_native = nullptr;
+                CHECK_DW_ERROR(dwImage_getNvMedia(&nvmedia_rgb_native, frame_rgb_native_));
 
                 sensor_msgs::Image &img_msg = *ros_img_ptr_;
                 std_msgs::Header header;
                 header.seq = count;
                 header.stamp = ros::Time::now();
                 img_msg.header = header;
-                img_msg.height = nvmedia_rgb_img_ptr->prop.height;
-                img_msg.width  = nvmedia_rgb_img_ptr->prop.width;
-                img_msg.encoding = sensor_msgs::image_encodings::RGBA8;
-                img_msg.step = img_msg.width * 4;
-                size_t img_size = img_msg.step * img_msg.height;
-                img_msg.data.resize(img_size);
 
+                // NVMedia görüntü kilitleniyor
                 NvMediaImageSurfaceMap surfaceMap;
-                if(NvMediaImageLock(nvmedia_rgb_img_ptr->img, NVMEDIA_IMAGE_ACCESS_READ, &surfaceMap) == NVMEDIA_STATUS_OK)
-                {
-                    unsigned char* buffer = (unsigned char*)surfaceMap.surface[0].mapping;
-                    memcpy(&img_msg.data[0], buffer, img_size);
+                if(NvMediaImageLock(nvmedia_rgb_native->img, NVMEDIA_IMAGE_ACCESS_READ, &surfaceMap) == NVMEDIA_STATUS_OK) {
+                    int nativeWidth = nvmedia_rgb_native->prop.width;
+                    int nativeHeight = nvmedia_rgb_native->prop.height;
+
+                    // OpenCV Mat oluşturuluyor (NVMedia görüntü verisinin sürekli bellek düzeni varsayılmıştır)
+                    cv::Mat nativeImage(nativeHeight, nativeWidth, CV_8UC4, surfaceMap.surface[0].mapping);
+                    cv::Mat resizedImage;
+                    cv::resize(nativeImage, resizedImage, cv::Size(640, 480));
+
+                    img_msg.height = resizedImage.rows;
+                    img_msg.width  = resizedImage.cols;
+                    img_msg.encoding = sensor_msgs::image_encodings::RGBA8;
+                    img_msg.step = resizedImage.cols * resizedImage.elemSize();
+                    size_t img_size = img_msg.step * img_msg.height;
+                    img_msg.data.resize(img_size);
+                    memcpy(&img_msg.data[0], resizedImage.data, img_size);
+
                     gmsl_pub_img_.publish(ros_img_ptr_);
-                    NvMediaImageUnlock(nvmedia_rgb_img_ptr->img);
+                    NvMediaImageUnlock(nvmedia_rgb_native->img);
                 }
 
                 // Temizlik
@@ -194,19 +176,15 @@ public:
                 ros::spinOnce();
                 loop_rate.sleep();
                 ++count;
-            }
-            catch(const std::runtime_error &e)
-            {
+            } catch(const std::runtime_error &e) {
                 ROS_ERROR("Runtime error: %s", e.what());
             }
         }
     }
 };
 
-int main(int argc, char **argv)
-{
-    try
-    {
+int main(int argc, char **argv) {
+    try {
         po::options_description desc{"Options"};
         desc.add_options()
             ("help,h", "Help screen")
@@ -220,14 +198,11 @@ int main(int argc, char **argv)
         po::store(parse_command_line(argc, argv, desc), args);
         po::notify(args);
 
-        // ROS initialization
         ros::init(argc, argv, "camera_gmsl");
 
         CameraGMSL cam(args);
         cam.publish();
-    }
-    catch(const std::runtime_error &e)
-    {
+    } catch(const std::runtime_error &e) {
         ROS_ERROR("Exception: %s", e.what());
         return -1;
     }
